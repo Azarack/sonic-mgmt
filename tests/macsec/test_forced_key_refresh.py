debf1d8eb25dd5d83e2fa1bd0e43ca4b067869d6
Azarack <jethro888222@yahoo.com>
2024-01-31 16:12:41 +0000

    last updates

--- tests/macsec/test_forced_key_refresh.py
+++ tests/macsec/test_forced_key_refresh.py
@@ -25,23 +25,40 @@ logger = logging.getLogger(__name__)
 # bgp_id_textfsm = "./bgp/templates/bgp_id.template"
 
 pytestmark = [
+    pytest.mark.macsec_required,
     pytest.mark.topology('t2')
 ]
 
 
 @pytest.fixture(scope='module')
-def setup(tbinfo, nbrhosts, duthosts, enum_frontend_dut_hostname, enum_rand_one_frontend_asic_index, request):
+def setup(tbinfo, nbrhosts, duthosts, enum_frontend_dut_hostname, request):
     # verify neighbors are type sonic
     if request.config.getoption("neighbor_type") != "sonic":
         pytest.skip("Neighbor type must be sonic")
-    duthost = duthosts[enum_frontend_dut_hostname]
-    asic_index = enum_rand_one_frontend_asic_index
-    namespace = duthost.get_namespace_from_asic_id(asic_index)
-    dut_asn = tbinfo['topo']['properties']['configuration_properties']['common']['dut_asn']
-    tor1 = duthost.shell("show lldp table")['stdout'].split("\n")[3].split()[1]
-    tor2 = duthost.shell("show lldp table")['stdout'].split("\n")[5].split()[1]
 
-    tor_neighbors = dict()
+    duthost = duthosts[enum_frontend_dut_hostname]
+    # asic_index = enum_rand_one_frontend_asic_index
+    # namespace = duthost.get_namespace_from_asic_id(asic_index)
+    dut_asn = tbinfo['topo']['properties']['configuration_properties']['common']['dut_asn']
+    lldp_table = duthost.shell("show lldp table")['stdout'].split("\n")[3].split()
+    logger.info("lldp table: {}".format(lldp_table))
+    neigh1 = lldp_table[1]
+    # neigh2 = duthost.shell("show lldp table")['stdout'].split("\n")[5].split()[1]
+    dut_int = lldp_table[0]
+    neigh_int = lldp_table[2]
+    if duthost.is_multi_asic:
+        asic_index = duthost.get_port_asic_instance(dut_int).asic_index
+    else:
+        asic_index = None
+
+    if nbrhosts[neigh1]["host"].is_multi_asic:
+        neigh_asic_index = nbrhosts[neigh1]["host"].get_port_asic_instance(neigh_int).asic_index
+    else:
+        neigh_asic_index = None
+
+    namespace = duthost.get_namespace_from_asic_id(asic_index)
+
+    # neighbors = dict()
     skip_hosts = duthost.get_asic_namespace_list()
     bgp_facts = duthost.bgp_facts(instance_id=asic_index)['ansible_facts']
     neigh_asn = dict()
@@ -49,86 +66,98 @@ def setup(tbinfo, nbrhosts, duthosts, enum_frontend_dut_hostname, enum_rand_one_
     # verify sessions are established and gather neighbor information
     for k, v in bgp_facts['bgp_neighbors'].items():
         if v['description'].lower() not in skip_hosts:
-            if v['description'] == tor1:
+            if v['description'] == neigh1:
                 if v['ip_version'] == 4:
                     neigh_ip_v4 = k
                     peer_group_v4 = v['peer group']
                 elif v['ip_version'] == 6:
                     neigh_ip_v6 = k
                     peer_group_v6 = v['peer group']
-            if v['description'] == tor2:
-                if v['ip_version'] == 4:
-                    neigh2_ip_v4 = k
-                elif v['ip_version'] == 6:
-                    neigh2_ip_v6 = k
-            assert v['state'] == 'established'
+            # if v['description'] == neigh2:
+            #     if v['ip_version'] == 4:
+            #         neigh2_ip_v4 = k
+            #     elif v['ip_version'] == 6:
+            #         neigh2_ip_v6 = k
+                assert v['state'] == 'established'
             neigh_asn[v['description']] = v['remote AS']
-            tor_neighbors[v['description']] = nbrhosts[v['description']]["host"]
+            # neighbors[v['description']] = nbrhosts[v['description']]["host"]
 
-    dut_ip_v4 = tbinfo['topo']['properties']['configuration'][tor1]['bgp']['peers'][dut_asn][0]
-    dut_ip_v6 = tbinfo['topo']['properties']['configuration'][tor1]['bgp']['peers'][dut_asn][1]
+    dut_ip_v4 = tbinfo['topo']['properties']['configuration'][neigh1]['bgp']['peers'][dut_asn][0]
+    dut_ip_v6 = tbinfo['topo']['properties']['configuration'][neigh1]['bgp']['peers'][dut_asn][1]
 
-    dut_ip_bgp_sum = duthost.shell('show ip bgp summary')['stdout']
-    neigh_ip_bgp_sum = nbrhosts[tor1]["host"].shell('show ip bgp summary')['stdout']
-    neigh2_ip_bgp_sum = nbrhosts[tor1]["host"].shell('show ip bgp summary')['stdout']
-    with open(bgp_id_textfsm) as template:
-        fsm = textfsm.TextFSM(template)
-        dut_bgp_id = fsm.ParseText(dut_ip_bgp_sum)[0][0]
-        neigh_bgp_id = fsm.ParseText(neigh_ip_bgp_sum)[1][0]
-        neigh2_bgp_id = fsm.ParseText(neigh2_ip_bgp_sum)[1][0]
+    # dut_ip_bgp_sum = duthost.shell('show ip bgp summary')['stdout']
+    # neigh_ip_bgp_sum = nbrhosts[tor1]["host"].shell('show ip bgp summary')['stdout']
+    # neigh2_ip_bgp_sum = nbrhosts[tor1]["host"].shell('show ip bgp summary')['stdout']
+    # with open(bgp_id_textfsm) as template:
+    #     fsm = textfsm.TextFSM(template)
+    #     dut_bgp_id = fsm.ParseText(dut_ip_bgp_sum)[0][0]
+    #     neigh_bgp_id = fsm.ParseText(neigh_ip_bgp_sum)[1][0]
+    #     neigh2_bgp_id = fsm.ParseText(neigh2_ip_bgp_sum)[1][0]
 
-    dut_ipv4_network = duthost.shell("show run bgp | grep 'ip prefix-list'")['stdout'].split()[6]
-    dut_ipv6_network = duthost.shell("show run bgp | grep 'ipv6 prefix-list'")['stdout'].split()[6]
-    neigh_ipv4_network = nbrhosts[tor1]["host"].shell("show run bgp | grep 'ip prefix-list'")['stdout'].split()[6]
-    neigh_ipv6_network = nbrhosts[tor1]["host"].shell("show run bgp | grep 'ipv6 prefix-list'")['stdout'].split()[6]
+    # dut_ipv4_network = duthost.shell("show run bgp | grep 'ip prefix-list'")['stdout'].split()[6]
+    # dut_ipv6_network = duthost.shell("show run bgp | grep 'ipv6 prefix-list'")['stdout'].split()[6]
+    # neigh_ipv4_network = nbrhosts[tor1]["host"].shell("show run bgp | grep 'ip prefix-list'")['stdout'].split()[6]
+    # neigh_ipv6_network = nbrhosts[tor1]["host"].shell("show run bgp | grep 'ipv6 prefix-list'")['stdout'].split()[6]
 
     setup_info = {
         'duthost': duthost,
-        'neighhost': tor_neighbors[tor1],
-        'neigh2host': tor_neighbors[tor2],
-        'tor1': tor1,
-        'tor2': tor2,
+        'neighhost': nbrhosts[neigh1],
+        # 'neigh2host': neighbors[neigh2],
+        'neigh1': neigh1,
+        # 'neigh2': neigh2,
         'dut_asn': dut_asn,
-        'neigh_asn': neigh_asn[tor1],
-        'neigh2_asn': neigh_asn[tor2],
+        'neigh_asn': neigh_asn[neigh1],
+        # 'neigh2_asn': neigh_asn[neigh2],
         'asn_dict':  neigh_asn,
-        'neighbors': tor_neighbors,
+        # 'neighbors': nbrhosts,
         'namespace': namespace,
         'dut_ip_v4': dut_ip_v4,
         'dut_ip_v6': dut_ip_v6,
         'neigh_ip_v4': neigh_ip_v4,
         'neigh_ip_v6': neigh_ip_v6,
-        'neigh2_ip_v4': neigh2_ip_v4,
-        'neigh2_ip_v6': neigh2_ip_v6,
+        # 'neigh2_ip_v4': neigh2_ip_v4,
+        # 'neigh2_ip_v6': neigh2_ip_v6,
         'peer_group_v4': peer_group_v4,
         'peer_group_v6': peer_group_v6,
         'asic_index': asic_index,
-        'dut_bgp_id': dut_bgp_id,
-        'neigh_bgp_id': neigh_bgp_id,
-        'neigh2_bgp_id': neigh2_bgp_id,
-        'dut_ipv4_network': dut_ipv4_network,
-        'dut_ipv6_network': dut_ipv6_network,
-        'neigh_ipv4_network': neigh_ipv4_network,
-        'neigh_ipv6_network': neigh_ipv6_network
+        'neigh_asic_index': neigh_asic_index,
+        'dut_int': dut_int,
+        # 'dut_bgp_id': dut_bgp_id,
+        # 'neigh_bgp_id': neigh_bgp_id,
+        # 'neigh2_bgp_id': neigh2_bgp_id,
+        # 'dut_ipv4_network': dut_ipv4_network,
+        # 'dut_ipv6_network': dut_ipv6_network,
+        # 'neigh_ipv4_network': neigh_ipv4_network,
+        # 'neigh_ipv6_network': neigh_ipv6_network
     }
 
     logger.info("DUT BGP Config: {}".format(duthost.shell("show run bgp", module_ignore_errors=True)['stdout']))
-    logger.info("Neighbor BGP Config: {}".format(nbrhosts[tor1]["host"].shell("show run bgp")['stdout']))
+    logger.info("Neighbor BGP Config: {}".format(nbrhosts[neigh1]["host"].shell("show run bgp")['stdout']))
     logger.info('Setup_info: {}'.format(setup_info))
 
     yield setup_info
 
     # restore config to original state
     config_reload(duthost)
-    config_reload(tor_neighbors[tor1], is_dut=False)
+    config_reload(nbrhosts[neigh1]["host"], is_dut=False)
+    time.sleep(80)
 
     # verify sessions are established
     bgp_facts = duthost.bgp_facts(instance_id=asic_index)['ansible_facts']
     for k, v in bgp_facts['bgp_neighbors'].items():
         if v['description'].lower() not in skip_hosts:
-            logger.debug(v['description'])
-            assert v['state'] == 'established'
+            logger.info(v['description'])
+            #assert v['state'] == 'established'
 
 
 def test_forced_key_refresh(setup):
-    cmd = 'vtysh -n {}'
+    ns = '-n ' + str(setup['asic_index']) if setup['asic_index'] is not None else ''
+    cmd = 'vtysh -n {} -c "test macsec mka rekey interface {}"'.format(ns, setup['dut_int'])
+
+    command_output = setup['duthost'].shell(cmd, module_ignore_errors=True)
+
+    if len(command_output["stdout_lines"]) != 0:
+        logger.error("Error when resetting macsec key")
+        return False
+    
+    logger.info("done w test")
